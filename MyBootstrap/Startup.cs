c2a57423085112b1a45d99762163d559a73f04ce
Pauli Sutelainen <pauli.sutelainen@gmail.com>
2016-09-08 06:02:17 +0300

    Add project files.

---
+++ MyBootstrap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBootstrap.Startup))]
+namespace MyBootstrap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
